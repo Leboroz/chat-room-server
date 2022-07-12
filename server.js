c7801e6cb0a0aa01ec882f4bb694d5d27f562753
Leonardo Albornoz <albornozleo95@gmail.com>
2022-07-12 14:38:41 -0400

    Configure the middleware to use pug
    Added a middleware using the use function from express
    to add the /views/pug/index directory. Additionally, I fixed the
    path to my views and passed a title and message variables.

--- server.js
+++ server.js
@@ -2,17 +2,18 @@
 require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
-const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const session = require('express-session');
+const passport = require('passport');
 
 const app = express();
 
-fccTesting(app); //For FCC testing purposes
+app.set('view engine', 'pug')
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.route('/').get((req, res) => {
-  res.render('Load your view here');
+  res.render(__dirname + '/views/pug', {title: 'Hello', message: 'Please login'});
 });
 
 const PORT = process.env.PORT || 3000;
